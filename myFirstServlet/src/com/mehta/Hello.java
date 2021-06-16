53e43aa79117a97d78c26cf6e5305cceed1c759e
User <User@DESKTOP-PVPDAOH>
2021-06-16 17:17:38 +0530

    my first project

---
+++ myFirstServlet/src/com/mehta/Hello.java
@@ -0,0 +1,31 @@
+package com.mehta;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class hello
+ */
+@WebServlet(name = "Demo", urlPatterns = { "/Hello" })
+public class Hello extends HttpServlet {
+	
+    public Hello() {}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html");	
+		PrintWriter out=response.getWriter();		
+		out.print("<html><body>");
+		out.print("<h3> Hello Servlet </h3>");
+		out.print("</body></html>");
+
+	}																			
+
+}
